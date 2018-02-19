32426e010b4f9ef8c817d982ddaf3e6e74ad5e7e
yujiakimoto <ya242@cornell.edu>
2018-02-19 15:18:05 -0500

    Fixed typo

--- automl/generate_matrix.sh
+++ automl/generate_matrix.sh
@@ -12,4 +12,4 @@ fi
 TIME=`date +%Y%m%d%H%M`
 
 ls ${DATA_DIR}/*.csv | xargs -i --max-procs=90 bash -c \
-"echo {}; python generate_vector.py "classification" {} --file=${JSON_FILE} --save_dir=${SAVE_DIR}/${TIME}"
+"echo {}; python generate_vector.py 'classification' {} --file=${JSON_FILE} --save_dir=${SAVE_DIR}/${TIME}"
