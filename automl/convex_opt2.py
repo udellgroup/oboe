43a3f9d8cc13d2089f0669f72647d2be3926bca1
Chengrun Yang <ychr93@gmail.com>
2018-03-08 21:13:32 -0500

    Yuji's updates

--- automl/convex_opt2.py
+++ automl/convex_opt2.py
@@ -22,7 +22,7 @@ def solve(t_predicted, t_max, Y):
          t_max (float):            maximum runtime of sampled model
          Y (np.ndarray):           matrix representing latent variable weights of error matrix
     Returns:
-        scipy.optimize.OptimizeResult: optimization result. Refer to https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.OptimizeResult.html#scipy.optimize.OptimizeResult for more information.
+        np.ndarray:                optimal vector v (not truncated to binary values)
     """
     def objective(v):
         sign, log_det = np.linalg.slogdet(Y @ np.diag(v) @ Y.T)
@@ -35,20 +35,20 @@ def solve(t_predicted, t_max, Y):
     v0 = np.full((n, ), 0.5)
     constraints = {'type': 'ineq', 'fun': constraint}
     v_opt = minimize(objective, v0, method='SLSQP', bounds=[(0, 1)] * n, constraints=constraints)
-    return v_opt
+    return v_opt.x
 
 
-def predict_runtime(shape, saved_model=None):
+def predict_runtime(size, saved_model=None):
     """Predict the runtime for each model setting on a dataset with given shape.
 
     Args:
-        shape (np.ndarray): 1-d array specifying dataset size as [n_rows, n_columns]
-        saved_model (str):  path to pre-trained model; defaults to None
+        size (np.ndarray): 1-d array specifying dataset size as [n_rows, n_columns]
+        saved_model (str): path to pre-trained model; defaults to None
     Returns:
-        np.ndarray:         1-d array of predicted runtimes
+        np.ndarray:        1-d array of predicted runtimes
     """
-    assert len(shape) == 2, "Dataset must be 2-dimensional."
-    shape = np.array(shape)
+    assert len(size) == 2, "Dataset must be 2-dimensional."
+    shape = np.array(size)
 
     if saved_model:
         with open(saved_model, 'rb') as file:
@@ -58,7 +58,7 @@ def predict_runtime(shape, saved_model=None):
     defaults_path = os.path.join(os.path.realpath(__file__), 'defaults')
     x_train = pd.read_csv(os.path.join(defaults_path, 'dataset_sizes.csv'))
     y_train = pd.read_csv(os.path.join(defaults_path, 'runtime_matrix.csv'))
-    model = RuntimePredictor(x_train, y_train)
+    model = RuntimePredictor(3, x_train, y_train)
     with open(os.path.join(defaults_path, 'runtime_predictor.pkl'), 'wb') as file:
         pickle.dump(model, file)
 
@@ -66,9 +66,35 @@ def predict_runtime(shape, saved_model=None):
 
 
 class RuntimePredictor:
-    def __init__(self, shapes, runtimes):
+    """Model that predicts the runtime for each model setting on a dataset with given shape. Performs polynomial
+    regression on n (# samples), p (# features), and log(n).
+
+    Attributes:
+        degree (int):   degree of polynomial basis function
+        n_models (int): number of model settings
+        models (list):  list of scikit-learn LinearRegression models
+    """
+    def __init__(self, degree, sizes, runtimes):
+        self.degree = degree
         self.n_models = runtimes.shape[1]
         self.models = (None, ) * self.n_models
+        self.fit(sizes, runtimes)
 
-    def predict(self, shape):
-        pass
+    def fit(self, sizes, runtimes):
+        """Fit polynomial regression on pre-recorded runtimes on datasets."""
+        assert sizes.shape[0] == runtimes.shape[0], "Dataset sizes and runtimes must be recorded on same datasets."
+        x_raw = np.concatenate((sizes, np.log(sizes[:, 0]).reshape(-1, 1)), axis=1)
+        x_train = PolynomialFeatures(self.degree).fit_transform(x_raw)
+
+        # train independent regression model to predict each runtime of each model setting
+        for i in range(self.n_models):
+            y_train = runtimes[:, i]
+            self.models[i] = LinearRegression().fit(x_train, y_train)
+
+    def predict(self, size):
+        """Predict runtime of all model settings on a dataset of given size."""
+        x_test = np.append(size, np.log(size[0]))
+        predictions = np.zeros(self.n_models)
+        for i in range(self.n_models):
+            predictions[i] = self.models[i].predict(x_test)
+        return predictions
