4aff1873335a70b9d389431f83ced39516a76b21
Chengrun Yang <ychr93@gmail.com>
2018-03-20 16:32:50 -0400

    add knapsack solver; formatting

---
+++ automl/knapsack.py
@@ -0,0 +1,18 @@
+#source: https://www.geeksforgeeks.org/knapsack-problem/, contributed by Bhavya Jain
+
+# A Dynamic Programming based Python Program for 0-1 Knapsack problem
+# Returns the maximum value that can be put in a knapsack of capacity W
+def knapsack(W, wt, val, n):
+    K = [[0 for x in range(W+1)] for x in range(n+1)]
+    
+    # Build table K[][] in bottom up manner
+    for i in range(n+1):
+        for w in range(W+1):
+            if i==0 or w==0:
+                K[i][w] = 0
+            elif wt[i-1] <= w:
+                K[i][w] = max(val[i-1] + K[i-1][w-wt[i-1]],  K[i-1][w])
+            else:
+                K[i][w] = K[i-1][w]
+
+return K[n][W]
