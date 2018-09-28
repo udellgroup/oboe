a94de0cfce1b60d016bf8db122fab8bb9e92d890
ychr93@gmail.com <ychr93@gmail.com>
2018-09-27 21:33:43 -0400

    update error matrix generation script

--- examples/error_matrix_generation/generate.sh
+++ examples/error_matrix_generation/generate.sh
@@ -1,1 +1,1 @@
-bash ../../automl/generate_matrix.sh -p classification -m generate -s results -d /data/cy438/error_matrix_generation -j ../../automl/defaults/classification.json -n 5
+bash ../../automl/generate_matrix.sh -p classification -m generate -s results -d . -j ../../automl/defaults/classification.json -n 5
